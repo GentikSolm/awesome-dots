0dd2f0663ffd7ba74cdc62e1f199e68330053967
Josh Brown <josh@mjbrowns.com>
2022-09-15 09:04:19 -0400

    fix: tokyonight

--- nvim/lua/imjosh/init.lua
+++ nvim/lua/imjosh/init.lua
@@ -3,5 +3,11 @@ require('imjosh.remap')
 require('imjosh.plugins')
 require('imjosh.plugconf')
 require("nvim-python-repl").setup()
+require("tokyonight").setup({
+    transparent = true,
+    styles = {
+        sidebars = "transparent"
+    }
+})
 vim.cmd('source ~/.config/nvim/lua/imjosh/coc.vim') 
 
