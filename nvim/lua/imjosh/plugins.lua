dabe450231cf8ef04dc6f6f59ad81a12f74d06fc
josh <josh@mjbrowns.com>
2022-11-01 20:46:58 -0400

    feat: add more nvim plugins for surround management

--- nvim/lua/imjosh/plugins.lua
+++ nvim/lua/imjosh/plugins.lua
@@ -10,6 +10,19 @@ return require('packer').startup(function()
         }
     }
 
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
+
+    use 'jiangmiao/auto-pairs'
+
+
     use {
         'nvim-telescope/telescope.nvim',
         requires = {
