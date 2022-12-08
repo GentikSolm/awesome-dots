a2f136a62ec8eacd775ef5860e5e26579734e2d5
Josh Brown <josh@mjbrowns.com>
2022-12-08 12:56:06 -0500

    fix: remove experimental backends from picom conf

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -359,7 +359,7 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- Startup Applications
 do
     local run_on_startup = {
-        "picom -b --experimental-backends --dbus --config ~/.config/picom/picom.conf",
+        "picom -b --dbus --config ~/.config/picom/picom.conf",
         "xrdb $HOME/.Xresources",
         config_dir .. "scripts/set_wallpapers /home/josh/Pictures/nordic-wallpapers/wallpapers",
     }
