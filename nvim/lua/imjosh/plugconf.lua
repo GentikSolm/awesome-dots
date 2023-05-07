8d510d55ea6758da9966df9542b6ab4b64907042
josh <josh@mjbrowns.com>
2023-05-07 17:27:22 -0400

    fix(nvim): fuzzy finder for wilder

--- nvim/lua/imjosh/plugconf.lua
+++ nvim/lua/imjosh/plugconf.lua
@@ -40,22 +40,17 @@ wilder.set_option('renderer', wilder.renderer_mux({
 wilder.set_option('pipeline', {
   wilder.branch(
     wilder.cmdline_pipeline({
-      -- sets the language to use, 'vim' and 'python' are supported
-      language = 'python',
-      -- 0 turns off fuzzy matching
-      -- 1 turns on fuzzy matching
-      -- 2 partial fuzzy matching (match does not have to begin with the same first letter)
-      fuzzy = 1,
+      fuzzy = 2,
+      fuzzy_filter = wilder.lua_fzy_filter(),
     }),
+    {
+      wilder.check(function(ctx, x) return x == '' end),
+      wilder.history(),
+    },
     wilder.python_search_pipeline({
-      -- can be set to wilder#python_fuzzy_delimiter_pattern() for stricter fuzzy matching
-      pattern = wilder.python_fuzzy_pattern(),
-      -- omit to get results in the order they appear in the buffer
-      sorter = wilder.python_difflib_sorter(),
-      -- can be set to 're2' for performance, requires pyre2 to be installed
-      -- see :h wilder#python_search() for more details
-      engine = 're',
+      pattern = wilder.python_fuzzy_pattern({
+        start_at_boundary = 0,
+      }),
     })
   ),
 })
-
