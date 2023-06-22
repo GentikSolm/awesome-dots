e9c06eca32a39652ede7fbdb528befedb6c66410
Josh Brown <josh@mjbrowns.com>
2023-06-21 20:53:25 -0400

    fix: remove hop binds

--- nvim/lua/imjosh/remap.lua
+++ nvim/lua/imjosh/remap.lua
@@ -30,17 +30,3 @@ nnoremap("<leader>G", ":Neotree float git_status<CR>")
 -- Misc helpers
 nnoremap("<leader>p", "\"0p<CR>")
 nnoremap("<leader>ra", ":%s/")
-
--- Hop
-nnoremap("<leader>hw", ":HopWord<CR>")
-nnoremap("<leader>hl", ":HopLineStart<CR>")
-nnoremap("<leader>hp", ":HopPattern<CR>")
-nnoremap("<leader>hf", ":HopWordCurrentLine<CR>")
-nnoremap("<leader>ha", ":HopAnywhereCurrentLine<CR>")
-nnoremap("<leader>hW", ":HopWordMW<CR>")
-vnoremap("<leader>hw", "<cmd>HopWord<CR>")
-vnoremap("<leader>hl", "<cmd>HopLineStart<CR>")
-vnoremap("<leader>hp", "<cmd>HopPattern<CR>")
-vnoremap("<leader>hf", "<cmd>HopWordCurrentLine<CR>")
-vnoremap("<leader>ha", "<cmd>HopAnywhereCurrentLine<CR>")
-vnoremap("<leader>hW", "<cmd>HopWordMW<CR>")
