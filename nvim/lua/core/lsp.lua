ac0de7d73d5ae3699e9ac9fa2a033e384b196884
josh <josh@mjbrowns.com>
2024-09-26 11:04:52 -0500

    feat: p10k config

--- nvim/lua/core/lsp.lua
+++ nvim/lua/core/lsp.lua
@@ -25,7 +25,7 @@ function OpenDiagnosticIfNoFloat()
 end
 
 vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = ".env",
+  pattern = "*.env",
   group = vim.api.nvim_create_augroup("__env", {clear=true}),
   callback = function(args)
     vim.diagnostic.disable(args.buf)
